12284aa2ea5ba336a94537bd634d92b63844b544
ReflextionsDev <reflextionsdev@gmail.com>
2022-06-26 18:21:31 -0400

    login/signup pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+
+
+import Login from "./pages/Signup";
+import Signup from "./pages/Login"
+
 function App() {
   return (
     <div className="App">
      Hello There!
+     <Login/>
+     <Signup/>
     </div>
   );
 }
---
+++ src/pages/Signup.js
@@ -0,0 +1,7 @@
+import React, { useEffect, useState, useContext } from 'react'
+
+export default function Login() {
+  return (
+    <div>Signup Page</div>
+  )
+}
