2d72da63048a1ab4777b75ec746fb295dfa453aa
ReflextionsDev <reflextionsdev@gmail.com>
2022-06-28 21:59:21 -0400

    replace signup form, using react-hook-form and axios

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,86 +1,51 @@
-import React, { useEffect, useState, useContext } from 'react'
+import axios from 'axios';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+// Frontend validation
+// Backend validation
 
 export default function Signup() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => {
+    console.log(data);
 
-
-  const [firstName, setFirstName] = useState("")
-  const [lastName, setLastName] = useState("")
-  const [username, setUsername] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+    axios.post('http://localhost:3001/users/create-user', data)
+      .then(
+        // if status code
+        response => {
+          if (response.status === 200) {
+            console.log('SUCCESS: ', response.data)
+          } else {
+            console.log('Something went wrong.', response)
+          }
+        }
+      )
+      .catch(e => console.log('CAUGHT ERROR', e.response.data.error))
+  }
+  console.log("Errors:", errors);
 
   return (
     <div>
       <h1>Create Account</h1>
 
-      <div>
-        <label>First Name: </label>
-        <input
-          id='firstname'
-          value={firstName}
-          onChange={e => setFirstName(e.target.value)}></input>
-      </div>
+      {/* {errors.password?.type === 'required' && "Password is required"} */}
 
-      {/* <div>{signupError && error.lastName != undefined ? `Error: ${error.lastName}` : ""}</div> */}
-
-      <div>
-        <label>Last Name: </label>
-        <input
-          id='lastname'
-          value={lastName}
-          onChange={e => setLastName(e.target.value)}></input>
-      </div>
-
-      {/* <div>{signupError && error.username != undefined ? `Error: ${error.username}` : ""}</div> */}
-
-      <div>
-        <label>User Name: </label>
-        <input
-          id='username'
-          value={username}
-          onChange={e => setUsername(e.target.value)}></input>
-      </div>
-
-      {/* <div>{signupError && error.email != undefined ? `Error: ${error.email}` : ""}</div> */}
-
-      {/* Email validation is lacking */}
-
-      <div>
-        <label>Email: </label>
-        <input
-          id='email'
-          value={email}
-          onChange={e => setEmail(e.target.value)}></input>
-      </div>
-
-      {/* <div>{signupError && error.password != undefined ? `Error: ${error.password}` : ""}</div> */}
-
-      <div>
-        <label>Password: </label>
-        <input
-          type='password'
-          id='password'
-          value={password}
-          onChange={e => setPassword(e.target.value)}></input>
-      </div>
-
-      <div>
-        <button
-          id="signup"
-          // onClick={e => dispatch(createUser(
-          //   {
-          //     firstName: firstName,
-          //     lastName: lastName,
-          //     username: username,
-          //     email: email,
-          //     password: password
-          //   }
-          // ))}
-        >Sign Up!</button>
-
-        {/* <div>{message}</div> */}
-      </div>
-
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" placeholder="First name" {...register("firstName", { required: true, maxLength: 100 })} />
+        <input type="text" placeholder="Last name" {...register("lastName", { required: true, maxLength: 100 })} />
+        <input type="text" placeholder="Username" {...register("username", { required: true, maxLength: 36 })} />
+        <input type="text" placeholder="Email" {...register("email", { required: true, pattern: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i })} />
+        <input type="password" placeholder="Pasword" {...register("password", { required: true, min: 8, maxLength: 256, pattern: /^.{24,}|(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/i })} />
+        <input type="submit" />
+      </form>
     </div>
-  )
+  );
 }
+
+{/* <div>{signupError && error.lastName != undefined ? `Error: ${error.lastName}` : ""}</div>
+<div>{signupError && error.username != undefined ? `Error: ${error.username}` : ""}</div>
+<div>{signupError && error.email != undefined ? `Error: ${error.email}` : ""}</div>
+Email validation is lacking
+<div>{signupError && error.password != undefined ? `Error: ${error.password}` : ""}</div>
+<div>{message}</div> */}
