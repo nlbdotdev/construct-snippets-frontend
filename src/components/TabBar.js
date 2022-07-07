d1025523e00695ed4843ca0ecdbd7bd73ac8f1f9
ReflextionsDev <reflextionsdev@gmail.com>
2022-07-06 20:46:38 -0400

    SegmentedControl content display

--- src/components/TabBar.js
+++ src/components/TabBar.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Tabs, SegmentedControl, ThemeIcon, Center, Box } from '@mantine/core';
+
+// The TabBar component builds responsive navigation tabs based on passed props using mantine components
+// On large displays, Tabs is used
+// On small displays, SegmentedControl is used
+// Display changing with Tabs is included with the component, so all data can just be passed into the childrens (Tabs.Tab). SegmentedControl is a bit more involved and needs to be controlled
 
 export default function TabBar(props) {
 
+
   // Destructure data from props
   const { tabData } = props
+
+  // Returns content for the current segment, pulling from tabData
+  const getSegmentContent = (value) => {
+    const tab = tabData.find(obj => (obj.value === value))
+    return tab.content
+  }
+
+  // ***Should pass in default tab as prop
+  // Segment Hooks
+  const [currentSegment, setCurrentSegment] = useState('functions')
+  const [segmentContent, setSegmentContent] = useState(getSegmentContent('all'))
+
+  // Controlled segment function, updates segment and content
+  const changeSegment = (value) => {
+    setCurrentSegment(value)
+    setSegmentContent(getSegmentContent(value))
+  }
+
+  // Accepts array of tab objects, return SegmentedControl data prop
+  const buildSegments = (tabArray) => {
+
+    let elems = tabArray.map(data => (
+      {
+        label: data.label,
+        value: data.value,
+        disabled: data.disabled,
+        content: 'yeet'
+      }
+    ))
+
+    return elems
+  }
 
   // Accepts an array of tab objects, keys: label, value, color, icon, disabled, content
   const buildTabs = (tabArray) => {
@@ -18,20 +56,6 @@ export default function TabBar(props) {
     return elems
   }
 
-  // Accepts array of tab objects, return SegmentedControl data prop
-  const buildSegments = (tabArray) => {
-
-    let elems = tabArray.map(data => (
-      {
-        label: data.label,
-        value: data.value,
-        disabled: data.disabled
-      }
-    ))
-
-    return elems
-  }
-
   // Build components
   const tabElements = buildTabs(tabData)
   const segmentData = buildSegments(tabData)
@@ -40,15 +64,23 @@ export default function TabBar(props) {
     <div>
       <h4>Tab Bar</h4>
 
+      {/* Need to make responsivly show one or the other */}
 
       <Tabs grow orientation="horizontal">
         {tabElements}
       </Tabs>
 
-      <SegmentedControl fullWidth orientation="vertical"
+      {/* Mobile */}
+
+      <SegmentedControl
+        fullWidth
+        orientation="vertical"
+        value={currentSegment}
+        onChange={e => changeSegment(e)}
         data={segmentData}
       />
 
+      {segmentContent}
 
     </div>
   )
