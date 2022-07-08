715b097276af0f2247d208d32442e858b1ad21c8
ReflextionsDev <reflextionsdev@gmail.com>
2022-07-08 18:48:05 -0400

    make TabPage responsive

--- src/components/TabPage.js
+++ src/components/TabPage.js
@@ -1,23 +1,32 @@
-import { Group, Container, ThemeIcon, useMantineTheme } from '@mantine/core'
+import { Group, Container, ThemeIcon, useMantineTheme, Stack, MediaQuery } from '@mantine/core'
 import React from 'react'
 
 // ++ Aside content should be sticky on desktop
 
-// Should accept props for main and aside
-// Needs to be responsive for mobile, display aside on top
+// Tab Page is a responsive page composed of main content, and an aside, which are passed in as props.
+// It's designed to be modular for displaying snippets content: main consisting of a content grid, and the aside being information or filter parameters 
 export default function TabPage(props) {
 
     const { main, aside } = props
-
-
     const theme = useMantineTheme()
 
     return (
+        <div>
+            {/* Large Display */}
+            <MediaQuery smallerThan="sm" styles={{ display: 'none' }}>
+                <Group align="flex-start">
+                    <div style={{ flex: 2 }}>{main}</div>
+                    <div style={{ flex: 1 }}>{aside}</div>
+                </Group>
+            </MediaQuery>
 
-        <Group height= '1020px' align="flex-start">
-            <div style={{flex: 2}}>{main}</div>
-            <div style={{flex: 1,  background: theme.colors.gray[3]}}>{aside}</div>
-        </Group>
-
+            {/* Mobile Display */}
+            <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+                <Stack py='md'>
+                    {aside}
+                    {main}
+                </Stack>
+            </MediaQuery>
+        </div>
     )
 }
