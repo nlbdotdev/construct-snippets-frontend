19305e8132fd7541ce0a6d15390745e9a65b5cf1
ReflextionsDev <reflextionsdev@gmail.com>
2022-08-04 01:15:12 -0400

    add NeedLogin buttons

--- src/components/NeedLogin.js
+++ src/components/NeedLogin.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { Text, Paper, Group, Button, Divider, Stack, Box } from '@mantine/core';
 
-
 // Add button to log in page
+// Add home button page
+
 export default function NeedLogin() {
     return (
         <Box sx={{ maxWidth: 576 }} mx="auto" style={{ position: 'relative' }}>
             <Paper radius="md" p="xl" shadow='md' withBorder >
-                <Text>You must be logged in to access this page.</Text>
+                <Stack>
+                    <Text>You must be logged in to access this page.</Text>
+                    <Group position="center" mt="sm" spacing="xl">
+                        <a href="/authentication"><Button>Ok, login</Button></a>
+                        <a href="/"><Button>Take me home, country roads</Button></a>
+                    </Group>
+                </Stack>
             </Paper>
         </Box>
     )
