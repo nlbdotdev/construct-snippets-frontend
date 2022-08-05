b01d73ee80b876c9367f0dc7d3c5ea3ea5fd0746
ReflextionsDev <reflextionsdev@gmail.com>
2022-08-04 22:18:33 -0400

    add snippets dummy data and formatting

--- src/components/Snippets/SnippetsGrid.js
+++ src/components/Snippets/SnippetsGrid.js
@@ -1,25 +1,57 @@
-import { SimpleGrid, Card, Badge, Text, Group, useMantineTheme, Image, Button } from '@mantine/core'
+import { SimpleGrid, Card, Badge, Text, Group, useMantineTheme, Image, Button, Stack } from '@mantine/core'
 import React from 'react'
 
+const data = [
+  {
+    title: 'Currency Formatting',
+    rating: 4.5,
+    downloads: 1232,
+    author: 'MyNameJeff',
+    updated: '6/12',
+    uploaded: '6/10/22',
+    type: 'function',
+    tags: ['money', 'essential', 'strings'],
+    desc: 'This is my description',
+    clipboard: '{"is-c3-clipboard-data":true,"type":"events","items":[{"functionName":"String_FormatMoney","functionDescription":"","functionCategory":"String","functionReturnType":"any","functionIsAsync":false,"functionParameters":[{"name":"money","type":"number","initialValue":"0","comment":""},{"name":"symbol","type":"string","initialValue":"$","comment":""}],"eventType":"function-block","conditions":[],"actions":[],"children":[{"eventType":"variable","name":"temp","type":"number","initialValue":"0","comment":"","isStatic":false,"isConstant":false},{"eventType":"variable","name":"temptext","type":"string","initialValue":"","comment":"","isStatic":false,"isConstant":false},{"eventType":"block","conditions":[],"actions":[{"id":"set-eventvar-value","objectClass":"System","parameters":{"variable":"temptext","value":"\"\""}},{"id":"set-eventvar-value","objectClass":"System","parameters":{"variable":"temp","value":"int(Money)"}}]'
+  }
+]
 
 
 export default function SnippetsGrid() {
 
   const theme = useMantineTheme()
-
+  let item = data[0]
 
   const testCard = <Card shadow="sm" p="lg">
 
-
-
     <Group position="apart" style={{ marginBottom: 5, marginTop: theme.spacing.sm }}>
-      <Text weight={500}>Norway Fjord Adventures</Text>
-      <Badge color="pink" variant="light">
-        On Sale
-      </Badge>
+      <Stack>
+        ICON
+      </Stack>
+
+      <Stack>
+
+
+        <Text weight={500}>{item.title}</Text>
+        <Group>
+
+          X {item.rating}
+          V {item.downloads}
+        </Group>
+
+      </Stack>
     </Group>
 
-    <Text size="sm">
+    <Stack>
+    </Stack>
+
+
+    <Text size="sm" align='left'>Author: {item.author}</Text>
+    <Text size="sm" align='left'>Updated: {item.updated}</Text>
+    <Text size="sm" align='left'>Uploaded: {item.uploaded}</Text>
+
+
+    {/* <Text size="sm">
       With Fjord Tours you can explore more of the magical fjord landscapes with tours and
       activities on and around the fjords of Norway
     </Text>
@@ -27,6 +59,10 @@ export default function SnippetsGrid() {
     <Button variant="light" color="blue" fullWidth style={{ marginTop: 14 }}>
       Book classic tour now
     </Button>
+
+    <Badge color="pink" variant="light">
+      On Sale
+    </Badge> */}
 
 
 
@@ -50,7 +86,7 @@ export default function SnippetsGrid() {
       {testCard}
       {testCard}
       {testCard}
-      
+
     </SimpleGrid>
   )
 }
