cf5c9719b87bea379be23c707cd6b6d132ea93be
ReflextionsDev <reflextionsdev@gmail.com>
2022-08-09 14:55:06 -0400

    snippet text

--- src/components/Snippets/SnippetsGrid.js
+++ src/components/Snippets/SnippetsGrid.js
@@ -14,7 +14,7 @@ const data = [
     type: 'function',
     tags: ['money', 'essential', 'strings'],
     desc: 'This is my description',
-    clipboard: '{"is-c3-clipboard-data":true,"type":"events","items":[{"functionName":"String_FormatMoney","functionDescription":"","functionCategory":"String","functionReturnType":"any","functionIsAsync":false,"functionParameters":[{"name":"money","type":"number","initialValue":"0","comment":""},{"name":"symbol","type":"string","initialValue":"$","comment":""}],"eventType":"function-block","conditions":[],"actions":[],"children":[{"eventType":"variable","name":"temp","type":"number","initialValue":"0","comment":"","isStatic":false,"isConstant":false},{"eventType":"variable","name":"temptext","type":"string","initialValue":"","comment":"","isStatic":false,"isConstant":false},{"eventType":"block","conditions":[],"actions":[{"id":"set-eventvar-value","objectClass":"System","parameters":{"variable":"temptext","value":"\"\""}},{"id":"set-eventvar-value","objectClass":"System","parameters":{"variable":"temp","value":"int(Money)"}}]'
+    clipboard: `{"is-c3-clipboard-data":true,"type":"events","items":[{"functionName":"Function1","functionDescription":"","functionCategory":"","functionReturnType":"none","functionIsAsync":false,"functionParameters":[],"eventType":"function-block","conditions":[],"actions":[{"id":"log","objectClass":"Browser","parameters":{"type":"log","message":"\"hello\""}}]}]}`
   }
 ]
 
@@ -27,77 +27,85 @@ export default function SnippetsGrid() {
   const testCard =
 
     <Card shadow="sm" p="lg"
-    
+      onClick={() => console.log('card pressed')}
+      sx={(theme) => ({
+        '&:hover': {
+          backgroundColor: theme.colors.gray[1],
+          cursor: 'pointer',
+        },
+      })}
     >
 
-      <UnstyledButton 
-      onClick={() => console.log('try focusing button with tab')}
-       spacing="0" m="0" p="0"
-        radius="120px"
-        border="xl"
-        sx={(theme) => ({
-          '&:hover': {
-            backgroundColor: theme.colors.gray[1],
-            cursor: 'pointer',
-          },
-        })}
-      
-      
-      >
-        {/* Top */}
-        <Group
-          position="left"
-          style={{ marginBottom: 5, marginTop: theme.spacing.sm }}>
-          <Stack>
-            <Container
-              size="xs" p="xs"
-              sx={(theme) => ({
-                backgroundColor: theme.colors.gray[3],
-                borderRadius: '100px',
-              })}
-            >
-              <MathFunction
-                size={32}
-              />
-            </Container>
-          </Stack>
-          <Stack spacing='xs'>
-            <Group>
-              <Text weight={500}>{item.title}</Text>
-            </Group>
-            <Group>
-              <Group spacing='5px'>
-                <Star size={24} />
-                {item.rating}
-              </Group>
-              <Group spacing='5px'>
-                <Download size={24} />
-                {item.downloads}
-              </Group>
-            </Group>
-          </Stack>
-        </Group>
-        {/* Body */}
-        <Stack spacing='0' my='md'>
-          <Text size="sm" align='left'>Author: {item.author}</Text>
-          <Text size="sm" align='left'>Updated: {item.updated}</Text>
-          <Text size="sm" align='left'>Uploaded: {item.uploaded}</Text>
+      {/* Top */}
+      <Group
+        position="left"
+        style={{ marginBottom: 5, marginTop: theme.spacing.sm }}>
+        <Stack>
+          <Container
+            size="xs" p="xs"
+            sx={(theme) => ({
+              backgroundColor: theme.colors.gray[3],
+              borderRadius: '100px',
+            })}
+          >
+            <MathFunction
+              size={32}
+            />
+          </Container>
         </Stack>
-      </UnstyledButton>
+        <Stack spacing='xs'>
+          <Group>
+            <Text weight={500}>{item.title}</Text>
+          </Group>
+          <Group>
+            <Group spacing='5px'>
+              <Star size={24} />
+              {item.rating}
+            </Group>
+            <Group spacing='5px'>
+              <Download size={24} />
+              {item.downloads}
+            </Group>
+          </Group>
+        </Stack>
+      </Group>
 
+      {/* Body */}
+      <Stack spacing='0' my='md'>
+        <Text size="sm" align='left'>Author: {item.author}</Text>
+        <Text size="sm" align='left'>Updated: {item.updated}</Text>
+        <Text size="sm" align='left'>Uploaded: {item.uploaded}</Text>
+      </Stack>
 
       {/* Bottom */}
       <Group position='right'>
-        <Button>
+        <Button
+          onClick={(event) => {
+            console.log('button pressed')
+            event.stopPropagation()
+            navigator.clipboard.writeText(item.clipboard);
+          }}
+        >
           <Copy size={24} />
         </Button>
       </Group>
-    </Card>
 
+    </Card>
 
   const newCard =
     <Card shadow="sm" p="lg"
-      sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+
+      onClick={() => console.log('new card pressed')}
+      sx={(theme) => ({
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        // '&:hover': {
+        //   backgroundColor: theme.colors.gray[1],
+        //   cursor: 'pointer',
+        // },
+      })}
+    >
 
       <ActionIcon size="xxl" radius="lg">
         <CirclePlus size={96} strokeWidth={2} />
@@ -105,13 +113,6 @@ export default function SnippetsGrid() {
 
     </Card>
 
-
-  const copyButton =
-    <Group position='right'>
-      <Button>
-        <Copy size={24} />
-      </Button>
-    </Group>
 
 
 
